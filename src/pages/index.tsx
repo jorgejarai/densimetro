b89f2a7e912e0fff89648b43f7c9663d7ddb4ddb
Jorge Jara <jorgejarainostroza@gmail.com>
2023-07-24 19:14:37 -0400

    Add link to GitHub repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import Stats from "@/components/Stats";
 
 import getData from "@/lib/getData";
 
+import { FaGithub } from "react-icons/fa";
+
 export default function Home({
   count,
   record,
@@ -28,6 +30,14 @@ export default function Home({
       <main className="bg-yellow-300 w-screen h-screen md:p-16 flex flex-col items-center">
         <Stats count={count} record={record} />
         <Confetti show={count === 0} />
+        <div className="mt-5">
+          <a
+            href="https://github.com/jorgejarai/densimetro"
+            className="text-4xl"
+          >
+            <FaGithub />
+          </a>
+        </div>
       </main>
     </>
   );
